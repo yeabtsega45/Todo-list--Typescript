7e9eeae08daf225dfb9355c7dfc962de4e7dcda8
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-30 00:44:30 +0300

    styling the page

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -5,15 +5,17 @@ function TodoList() {
     <div className="h-screen flex flex-col justify-center items-center">
       <h1>Todo List</h1>
       <ul>
-        <li></li>
-        <li></li>
+        <li className="mt-4"></li>
+        <li className="mt-4"></li>
       </ul>
       <input
         type="text"
         placeholder="Add todo item"
-        className="h-9 w-40 bg-white rounded-md p-3 border-[1px] border-solid border-white ml-3 mt-10"
+        className="h-9 w-[165px] bg-white rounded-md p-3 border-[1px] border-solid border-white ml-3 mt-10"
       />
-      <button>Add</button>
+      <button className="h-8 w-[60px] bg-blue-800 text-white text-base mt-3 cursor-pointer">
+        Add
+      </button>
     </div>
   );
 }
