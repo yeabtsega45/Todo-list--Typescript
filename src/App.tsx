9c871cab57e2fc88cc57dd88e0f23819a2756647
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-29 23:59:49 +0300

    creating UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TodoList from "./TodoList";
 
 function App() {
   return (
     <div>
-      <div></div>
+      <TodoList />
     </div>
   );
 }
---
+++ src/TodoList.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function TodoList() {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+      <input type="text" placeholder="Add todo item" />
+      <button>Add</button>
+    </div>
+  );
+}
+
+export default TodoList;
