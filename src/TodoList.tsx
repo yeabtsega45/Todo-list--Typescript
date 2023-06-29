8a7633128c1a77475d973b25cf9234b248a10150
yeabtsega45 <yabtsega2022@gmail.com>
2023-06-30 00:33:21 +0300

    styling the page

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -2,13 +2,17 @@ import React from "react";
 
 function TodoList() {
   return (
-    <div>
+    <div className="h-screen flex flex-col justify-center items-center">
       <h1>Todo List</h1>
       <ul>
         <li></li>
         <li></li>
       </ul>
-      <input type="text" placeholder="Add todo item" />
+      <input
+        type="text"
+        placeholder="Add todo item"
+        className="h-9 w-40 bg-white rounded-md p-3 border-[1px] border-solid border-white ml-3 mt-10"
+      />
       <button>Add</button>
     </div>
   );
